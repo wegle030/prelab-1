a1d7f5415e75cd26cfe14b6dcf9208bbd163c35d
wegle030 <46872268+wegle030@users.noreply.github.com>
2020-08-31 19:57:10 -0500

    Update wrap_contents.sh

--- simple_example/wrap_contents.sh
+++ simple_example/wrap_contents.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ $# -eq 3 ];then
+	#Sets the contents of the files to variables to pipe them into the new file
 	header=$(cat $2_header.html )
 	footer=$(cat $2_footer.html)
 	body=$(cat $1)
+	#Creates new file with a correct name and contents
 	cat > $3 <<- _EOF_
 	$header
 	$body
